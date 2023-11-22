574f0a09242f41ce8d7524e1a012737cee1ccd97
DevOps/Practice <DecOps@example.com>
2023-11-22 12:58:59 +0530

    message

--- vars/dockerBuild.groovy
+++ vars/dockerBuild.groovy
@@ -1,12 +1,10 @@
-
-
 def call(String dockerHubUsername, String imageName) {
     // Build the Docker image
-    sh "docker build --build-arg REACT_APP_RAPID_API_KEY=3fe354f5eamsh37c7e31ef2f57c2p129f7fjsnbfdb052f67f5-t ${imageName} ."
+    sh "docker build --build-arg REACT_APP_RAPID_API_KEY=3fe354f5eamsh37c7e31ef2f57c2p129f7fjsnbfdb052f67f5 -t ${imageName} ."
      // Tag the Docker image
     sh "docker tag ${imageName} ${dockerHubUsername}/${imageName}:latest"
     // Push the Docker image
-    withDockerRegistry([url: 'https://registry.hub.docker.com/v1/', credentialsId: 'docker']) {
+    withDockerRegistry([url: 'https://index.docker.io/v1/', credentialsId: 'docker']) {
         sh "docker push ${dockerHubUsername}/${imageName}:latest"
     }
 }
